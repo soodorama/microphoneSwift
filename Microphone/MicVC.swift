1d63b35a53e80729462577ebffe9c7644e33978a
Neil Sood <soodorama@gmail.com>
2018-09-25 00:20:53 -0700

    let's make this happen

---
+++ Microphone/MicVC.swift
@@ -0,0 +1,29 @@
+//
+//  MicVC.swift
+//  Microphone
+//
+//  Created by Neil Sood on 9/25/18.
+//  Copyright © 2018 Neil Sood. All rights reserved.
+//
+
+import UIKit
+
+class MicVC: UIViewController {
+    
+    @IBOutlet weak var switchButton: UIButton!
+    
+    @IBAction func switchPressed(_ sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("YEAHHHH BITCH")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+}
