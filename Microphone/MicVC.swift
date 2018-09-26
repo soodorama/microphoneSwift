ae999f4cb79c52c8fea79e606a9baede2041b4d3
Neil Sood <soodorama@gmail.com>
2018-09-26 15:49:26 -0700

    starting over without capturesession

--- Microphone/MicVC.swift
+++ Microphone/MicVC.swift
@@ -9,92 +9,43 @@
 import UIKit
 import AVFoundation
 
-class MicVC: UIViewController, AVCaptureAudioDataOutputSampleBufferDelegate {
+class MicVC: UIViewController {
     
     @IBOutlet weak var offButton: UIButton!
     @IBOutlet weak var onButton: UIButton!
     
-//    var isSwitched = false
     let onColor = UIColor(red: 142/255, green: 17/255, blue: 7/255, alpha: 1.0)
-//    var player: AVAudioPlayer?
-    
-    
-    let settings = [
-        AVFormatIDKey: kAudioFormatMPEG4AAC,
-        AVNumberOfChannelsKey : 1,
-        AVSampleRateKey : 44100]
-    let captureSession = AVCaptureSession()
-    
-    let queue = DispatchQueue(label: "AudioSessionQueue", attributes: [])
-    let captureDevice = AVCaptureDevice.default(for: AVMediaType.audio)
-    var audioInput : AVCaptureDeviceInput? = nil
-    var audioOutput : AVCaptureAudioDataOutput? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        captureSession.automaticallyConfiguresApplicationAudioSession = false
         onButton.backgroundColor = .orange
         offButton.backgroundColor = .gray
-        
         onButton.layer.cornerRadius = 10
         offButton.layer.cornerRadius = 10
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-    
-    func captureOutput(_ output: AVCaptureOutput,
-                       didOutput sampleBuffer: CMSampleBuffer,
-                       from connection: AVCaptureConnection) {
-        output.connection(with: AVMediaType(rawValue: AVAudioSessionPortBuiltInSpeaker))
-//        print("why can't i figure this ish out")
-        print("Audio data received")
-    }
-    
-    @IBAction func stopPressed(_ sender: UIButton) {
-        onButton.backgroundColor = .orange
-        offButton.backgroundColor = .gray
         
-        captureSession.stopRunning()
-        print("Stop")
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSessionCategoryPlayback)
+        } catch {
+            print("Setting category to AVAudioSessionCategoryPlayback failed.")
+        }
+        
+        print("Inputs",audioSession.availableInputs)
+        print("input datasource",audioSession.inputDataSource)
+        print("output datasource",audioSession.outputDataSource)
+        
     }
     
     @IBAction func onPressed(_ sender: UIButton) {
-        
         onButton.backgroundColor = .white
         offButton.backgroundColor = onColor
-    
-        
-        do {
-            try captureDevice?.lockForConfiguration()
-            audioInput = try AVCaptureDeviceInput(device: captureDevice!)
-            captureDevice?.unlockForConfiguration()
-            print(captureDevice!)
-            audioOutput = AVCaptureAudioDataOutput()
-            audioOutput?.setSampleBufferDelegate(self, queue: queue)
-//                audioOutput?.audioSettings = settings
-        } catch {
-            print("Capture devices could not be set")
-            print(error.localizedDescription)
-        }
-        
-        if audioInput != nil && audioOutput != nil {
-            captureSession.beginConfiguration()
-            if (captureSession.canAddInput(audioInput!)) {
-                captureSession.addInput(audioInput!)
-            } else {
-                print("cannot add input")
-            }
-            if (captureSession.canAddOutput(audioOutput!)) {
-                captureSession.addOutput(audioOutput!)
-            } else {
-                print("cannot add output")
-            }
-            captureSession.commitConfiguration()
-            
-            print("Starting capture session")
-            captureSession.startRunning()
-        }
+        print("On")
     }
+    
+    @IBAction func offPressed(_ sender: UIButton) {
+        onButton.backgroundColor = .orange
+        offButton.backgroundColor = .gray
+        print("Off")
+    }
+    
 }
